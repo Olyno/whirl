d3b38e11530c56968b893352ef979f2888320548
Olyno <olyno.dev@gmail.com>
2022-01-05 16:42:28 +0100

    chore: fix project workflow

--- utils/publish.js
+++ utils/publish.js
@@ -1,9 +1,23 @@
 const config = require('../src/whirl.config.json')
+const path = require('path')
 const fs = require('fs')
-const sass = require('node-sass')
+const fsp = require('fs-extra')
+const sass = require('sass')
 const postcss = require('postcss')
 const autoprefixer = require('autoprefixer')
-const processStyles = name => {
+
+const distDir = path.join(process.cwd(), 'dist')
+const defaultIndex = path.join(distDir, 'index.js')
+
+fsp.ensureDirSync(distDir)
+fsp.ensureDirSync(path.join(distDir, 'css'))
+
+fsp.writeFile(
+  defaultIndex,
+  "throw new Error('Do not import whirl directly. Import only css/sass you need!')"
+)
+
+const processStyles = async (name) => {
   sass.render(
     {
       file: `${process.cwd()}/src/whirls/${name}.scss`,
@@ -13,9 +27,9 @@ const processStyles = name => {
       postcss([autoprefixer])
         // Necessary to pass "undefined" to avoid warnings
         .process(result.css.toString(), { from: undefined })
-        .then(result => {
-          result.warnings().forEach(w => console.warn(w))
-          fs.writeFileSync(
+        .then((result) => {
+          result.warnings().forEach((w) => console.warn(w))
+          return fsp.writeFile(
             `${process.cwd()}/${
               process.env.FOR_PACKAGE && process.env.FOR_PACKAGE === 'true'
                 ? ''
@@ -27,6 +41,9 @@ const processStyles = name => {
     }
   )
 }
+
 for (const whirl of config.whirls) {
   if (whirl.active) processStyles(whirl.name)
 }
+
+fsp.copy(path.join(process.cwd(), 'src/whirls'), path.join(distDir, 'sass'))
