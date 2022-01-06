1a1752ba1653bb2cb07d3ec3e66f8def2344fd17
Olyno <olyno.dev@gmail.com>
2022-01-06 01:21:33 +0100

    build: fix base path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import WindiCSS from 'vite-plugin-windicss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), WindiCSS()],
+  base: process.env.NODE_ENV === 'dev' ? '/' : '/whirl/',
   build: {
     outDir: 'build',
   },
